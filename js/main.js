6317aab249d7c3af64f71fb3d9b2bd44065c98ae
quedongdong <qdd741273128@gmail.com>
2016-12-15 12:22:37 +0800

    tmp

--- js/main.js
+++ js/main.js
@@ -1,43 +1,49 @@
 var map;
 var marker;
+var xiAnLatlng = {lat:34.31621838080741, lng:108.951416015625};
 
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: -34.397, lng: 150.644},
-		zoom: 8
+		center:xiAnLatlng,
+		zoom: 4
 	});
 
 	map.addListener('click', function(e) {
-		placeMarkerAndPanTo(e.latLng, map);
+		placeMarkerTo(e.latLng, map, "image/ic_finger_marker.png");
+		onPositionChoosed(latLng.lat(), latLng.lng(), "test");
+		delayPanTo(e.latLng, map, 500);
 	});
 }
 
-function placeMarkerAndPanTo(latLng, map) {
+function delayPanTo(latLng, map, dayduring){
+	window.setTimeout(function() {
+		map.panTo(latLng);
+	}, during);
+}
+
+function placeMarkerTo(latLng, map, iconPath){
+	console.dir(latLng);
+
 	if(marker!=null)
 		marker.setMap(null);
 	marker = new google.maps.Marker({
 		position: latLng,
 		map: map,
-		icon: "image/ic_finger_marker.png"
+		icon: iconPath
 	});
-
-	console.dir(latLng);
-
-	window.setTimeout(function() {
-		map.panTo(latLng);
-	}, 500);
-
-	onPositionChoosed(latLng.lat(), latLng.lng(), "test");
 }
 
 //--------------------------from java-------------------------
 function moveTo(latitude,longitude) {
-	var myLatlng = {lat: latitude, lng: longitude};
-	map.panTo(myLatlng);
+	console.log(latitude+"   "+longitude);
+	var latlng = {lat: latitude, lng: longitude};
+	map.panTo(latlng);
+	placeMarkerTo(latlng, map, "image/ic_map_mark.png");
+	map.setZoom(19);
 } 
 
 //-------------------------to java-------------------
 function onPositionChoosed(latitude, longitude, address) {
-	// console.log(latitude+""+longitude);
-	window.control.onPositionChoosed(latitude, longitude, address);
+	console.log(latitude+"   "+longitude);
+	// window.control.onPositionChoosed(latitude, longitude, address);
 }
