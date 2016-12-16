7ba5cc44e20a8cc6f3c5d5a1b55a45dff1bba179
quedongdong <qdd741273128@gmail.com>
2016-12-16 08:42:41 +0800

    tmp

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ var xiAnLatlng = {lat:34.31621838080741, lng:108.951416015625};
 
 function initMap() {
 	geocoder = new google.maps.Geocoder;
-
 	map = new google.maps.Map(document.getElementById('map'), {
 		center:xiAnLatlng,
 		zoom: 4,
@@ -15,6 +14,40 @@ function initMap() {
 		zoomControl:false,
 		clickableIcons:false,
 	});
+
+	var input = document.getElementById('pac-input');
+	var autocomplete = new google.maps.places.Autocomplete(input);
+	autocomplete.bindTo('bounds', map);
+
+	autocomplete.addListener('place_changed', function() {
+		var place = autocomplete.getPlace();
+		if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+        }
+
+          // If the place has a geometry, then present it on a map.
+          if (place.geometry.viewport) {
+          	map.fitBounds(place.geometry.viewport);
+          } else {
+          	map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+        }
+
+        var address = '';
+        if (place.address_components) {
+        	address = [
+        	(place.address_components[0] && place.address_components[0].short_name || ''),
+        	(place.address_components[1] && place.address_components[1].short_name || ''),
+        	(place.address_components[2] && place.address_components[2].short_name || '')
+        	].join(' ');
+        }
+        console.dir(place);
+    });
+
+	
 
 	map.addListener('click', function(e) {
 		placeMarkerTo(e.latLng, map, "image/ic_finger_marker.png");
@@ -64,6 +97,11 @@ function moveTo(latitude,longitude) {
 	placeMarkerTo(latlng, map, "image/ic_map_mark.png");
 	map.setZoom(19);
 	geocodeLatLng(geocoder,latlng)
+} 
+
+function zoom(delt) {
+	var zoom = map.getZoom();
+	map.setZoom(zoom+delt);
 } 
 
 //-------------------------to java-------------------
